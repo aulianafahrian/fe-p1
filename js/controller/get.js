9d33ba7c580e92b5ae3b427a436db09e5a86d7c5
aulianafahrian <aulianafahrian@gmail.com>
2023-04-01 13:46:10 +0700

    s

--- js/controller/get.js
+++ js/controller/get.js
@@ -12,9 +12,7 @@ function isiRow(value) {
     .replace("#DOSENPENGUJI#", value.location)
     .replace("#JUDUL#", value.checkin)
     .replace("#TANGGALSIDANG#", value.biodata.hari_kerja)
-    .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA")
-    .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK")
-    .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMMKELUAR")
+
     .replace("#WARNA#", getRandomColor())
     .replace(/#WARNALOGO#/g, getRandomColorName());
   addInner("iniTabel", content);
