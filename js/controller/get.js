2be51ef391479e78243f465cb9752ffcac87e19c
aulianafahrian <aulianafahrian@gmail.com>
2023-04-03 07:32:57 +0700

    ss

--- js/controller/get.js
+++ js/controller/get.js
@@ -8,8 +8,8 @@ function isiRow(value) {
   let content = 
   isiTabel.replace("#NAMA#", value.biodata_mahasiswa.nama)
           .replace("#NPM#", value.biodata_mahasiswa.npm)
-          .replace("#JURUSAN#", value.biodata_mahasiswa.jurusan)
-          .replace("#PRODI#", value.biodata_mahasiswa.prodi)
+          .replace("#JURUSAN#", value.biodata_mahasiswa.jurusan.nama)
+          .replace("#PRODI#", value.biodata_mahasiswa.prodi.nama)
           .replace("#DOSENPEMBIMBING#", value.dosen_pembimbing.nama)
           .replace("#DOSENPENGUJI#", value.dosen_penguji.nama)
           .replace("#JUDUL#", value.judul)
