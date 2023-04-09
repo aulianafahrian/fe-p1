5b6f95e65e9729518f3c73a883dcf5d1521a0519
aulianafahrian <aulianafahrian@gmail.com>
2023-04-09 21:15:28 +0700

    ss

--- js/temp/table.js
+++ js/temp/table.js
@@ -1,7 +1,7 @@
 export let isiTabel = 
 `
 <tr class="h-18 border-b border-coolGray-100">
-    <th class="whitespace-nowrap px-4 bg-white text-left">
+    <th class="whitespace-normal px-4 bg-white text-left">
         <div class="flex items-left -m-2">
             <div class="w-auto p-2">
                 <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
@@ -9,12 +9,12 @@ export let isiTabel =
             </div>
         </div>
     </th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#JURUSAN#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#PRODI#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#DOSENPEMBIMBING#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#DOSENPENGUJI#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#JUDUL#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#TANGGALSIDANG#</th>
+    <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#JURUSAN#</th>
+    <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#PRODI#</th>
+    <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#DOSENPEMBIMBING#</th>
+    <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#DOSENPENGUJI#</th>
+    <th class="whitespace-normal px-4 bg-white text-sm font-medium text-#col#-500 text-left">#JUDUL#</th>
+    <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#TANGGALSIDANG#</th>
     </th>
 </tr>
 `
