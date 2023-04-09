04b32a8d9b2112ba5b35397014cbd7856a32c7fd
aulianafahrian <aulianafahrian@gmail.com>
2023-04-09 23:34:53 +0700

    ss

--- js/temp/table.js
+++ js/temp/table.js
@@ -3,11 +3,8 @@ export let isiTabel =
 <tr class="h-18 border-b border-coolGray-100">
     <th class="whitespace-normal px-4 bg-white text-left">
         <div class="flex items-left -m-2">
-            <div class="w-auto p-1">
+            <div class="w-auto p-2">
                 <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
-            </div>
-            </br>
-            <div class="w-auto p-1">
                 <p class="text-xs font-medium text-coolGray-800">#NPM#</p>
             </div>
         </div>
