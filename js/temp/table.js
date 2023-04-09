a2877970c008835eebf51f31fbfd60308834cdad
aulianafahrian <aulianafahrian@gmail.com>
2023-04-09 22:37:33 +0700

    sss

--- js/temp/table.js
+++ js/temp/table.js
@@ -44,7 +44,13 @@ export let isiTabelmhs =
 export let isiTabeldsn = 
 `
 <tr class="h-18 border-b border-coolGray-100">
-    <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#NAMA#</th>
+<th class="whitespace-normal px-4 bg-white text-left">
+<div class="flex items-left -m-2">
+    <div class="w-auto p-2">
+        <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
+    </div>
+</div>
+</th>
     <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#NID#</th>
     <th class="whitespace-normal px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#PRODI#</th>
     </th>
